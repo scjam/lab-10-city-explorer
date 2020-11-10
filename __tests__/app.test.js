8cbe6ecef62fb83110ffac0940b44dcf69ae8991
Sydney James <sydneyjames7@gmail.com>
2020-11-09 18:17:49 -0800

    added hiking munge

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,64 +1,41 @@
 require('dotenv').config();
 
-const { execSync } = require('child_process');
-
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
-const client = require('../lib/client');
+const { mungeLocation } = require('../lib/utils.js');
 
 describe('app routes', () => {
   describe('routes', () => {
-    let token;
-  
-    beforeAll(async done => {
-      execSync('npm run setup-db');
-  
-      client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
-      token = signInData.body.token;
-  
-      return done();
+
+    test('returns location', () => {
+
+      const longLocation = [{
+        'place_id': '282983083',
+        'licence': 'https://locationiq.com/attribution',
+        'osm_type': 'relation',
+        'osm_id': '186579',
+        'boundingbox': [
+          '45.432536',
+          '45.6528812',
+          '-122.8367489',
+          '-122.4720252'
+        ],
+        'lat': '45.5202471',
+        'lon': '-122.6741949',
+        'display_name': 'Portland, Multnomah County, Oregon, USA',
+        'class': 'place',
+        'type': 'city',
+        'importance': 0.75356571743377,
+        'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+      }];
+
+      const expectation = 
+    {
+      formatted_query: 'Portland, Multnomah County, Oregon, USA',
+      latitude: '45.5202471',
+      longitude: '-122.6741949'
+    };
+
+      const result = mungeLocation(longLocation);
+      expect(result).toEqual(expectation);
     });
-  
-    afterAll(done => {
-      return client.end(done);
-    });
-
-  test('returns animals', async() => {
-
-    const expectation = [
-      {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
-      },
-      {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
-      }
-    ];
-
-    const data = await fakeRequest(app)
-      .get('/animals')
-      .expect('Content-Type', /json/)
-      .expect(200);
-
-    expect(data.body).toEqual(expectation);
   });
 });
